46328b89b74bdfe566e94802150817d975229daa
Gustavo Garcia <82327702+gustavogarciadev@users.noreply.github.com>
2021-07-30 12:17:29 -0300

    update-aula-objetos

--- java-script-type-script/objeto.js
+++ java-script-type-script/objeto.js
@@ -7,5 +7,10 @@ function criaPessoa (nome,sobrenome,idade){
 }
 
 const pessoa1 = criaPessoa('gustavo','garcia','23');
+const pessoa2 = criaPessoa('eduarda','fraga','20');
+const pessoa3 = criaPessoa('raquel','w','26');
+const pessoa4 = criaPessoa('mario','junior','65');
 
 console.log(pessoa1);
+console.log(pessoa1.nome);
+console.log(pessoa1.nome, pessoa2.nome, pessoa3.nome);
